3778e1d08a41e4cead4b10fd6d4615251a7c92c6
Alex Rix <alexanderjrix@gmail.com>
2017-01-07 16:26:53 -0800

    ??

--- kernels/calc_acc.cu
+++ kernels/calc_acc.cu
@@ -3,7 +3,7 @@
 __global__
 void calc_acc(double * pos , double * acc, double * mass,  const int n , const unsigned int start, const unsigned int end, const unsigned int rank) {
   // get index of particles
-  size_t i = threadIdx.x + blockDim.x * blockIdx.x + start;
+  int i = threadIdx.x + blockDim.x * blockIdx.x + start;
   double ax = 0, ay = 0, az = 0;
   // make sure we are not acccessing memory that is OOB
   if (i < end) {
